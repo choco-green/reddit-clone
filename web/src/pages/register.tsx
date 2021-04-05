28420de5273af4c750033ca6961a30f47b3d53ce
ChocoGreen <33989421+choco-green@users.noreply.github.com>
2021-04-05 17:56:09 +0100

    first commit

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,34 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import React from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+	return (
+		<Wrapper variant="small">
+			<Formik
+				initialValues={{ username: "", password: "" }}
+				onSubmit={(values) => {
+					console.log(values);
+				}}
+			>
+				{(props) => (
+					<Form>
+						<InputField name="username" placeholder="username" label="Username" />
+						<Box mt={4}>
+							<InputField name="password" placeholder="password" label="Password" type="password" />
+						</Box>
+						<Button mt={4} type="submit" isLoading={props.isSubmitting} colorScheme="teal">
+							Register
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Wrapper>
+	);
+};
+
+export default Register;
